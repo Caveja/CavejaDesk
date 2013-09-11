616e43e4e4cde1489654d1b1e2004f99e538c5fd
Davide Bellettini <davide.bellettini@gmail.com>
2013-09-11 12:46:41 +0200

    Adding User class

--- src/Caveja/Bundle/DeskBundle/Entity/User.php
+++ src/Caveja/Bundle/DeskBundle/Entity/User.php
@@ -4,6 +4,16 @@ namespace Caveja\Bundle\DeskBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
 class User extends BaseUser
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 }
