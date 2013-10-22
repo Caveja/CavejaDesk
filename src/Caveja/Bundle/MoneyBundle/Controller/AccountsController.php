7e10dd2b1d907fa4c3850f35c4db707b98139f5f
Davide Bellettini <davide.bellettini@gmail.com>
2013-10-22 11:53:18 +0200

    Stubbing Accounts REST controller

---
+++ src/Caveja/Bundle/MoneyBundle/Controller/AccountsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: davide
+ * Date: 22/10/13
+ * Time: 11.52
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Caveja\Bundle\MoneyBundle\Controller;
+
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class AccountsController extends FOSRestController
+{
+}
