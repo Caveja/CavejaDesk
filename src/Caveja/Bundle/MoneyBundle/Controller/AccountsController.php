7d5a8153ac0056ce180bb0f297aae261d9d7c7eb
Davide Bellettini <davide.bellettini@gmail.com>
2013-10-22 12:35:31 +0200

    Adding account creation

--- src/Caveja/Bundle/MoneyBundle/Controller/AccountsController.php
+++ src/Caveja/Bundle/MoneyBundle/Controller/AccountsController.php
@@ -2,8 +2,24 @@
 
 namespace Caveja\Bundle\MoneyBundle\Controller;
 
+use Caveja\Bundle\MoneyBundle\Entity\Account;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AccountsController extends FOSRestController
 {
+    public function postAccountsAction(Request $request)
+    {
+        $om = $this->getDoctrine()->getManagerForClass('Caveja\\Bundle\\MoneyBundle\\Entity\\Account');
+
+        $account = new Account();
+        $account->setName($request->request->get('name'));
+
+        $om->persist($account);
+        $om->flush();
+
+        $view = $this->view(['id' => $account->getId()], 200);
+
+        return $this->handleView($view);
+    }
 }
