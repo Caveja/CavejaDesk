c9a86731c3b89bfc6cd0fa95b86049637e04c000
Davide Bellettini <davide.bellettini@gmail.com>
2013-10-22 11:54:17 +0200

    Removing phpstorm header

--- src/Caveja/Bundle/MoneyBundle/Controller/AccountsController.php
+++ src/Caveja/Bundle/MoneyBundle/Controller/AccountsController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: davide
- * Date: 22/10/13
- * Time: 11.52
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Caveja\Bundle\MoneyBundle\Controller;
 
