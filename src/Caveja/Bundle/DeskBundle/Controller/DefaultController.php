e2db12fb6b147a997709fe0ac23991643ef7fb09
Davide Bellettini <davide.bellettini@gmail.com>
2014-01-16 14:25:44 +0100

    Redirecting to money home page, which will be developed first

--- src/Caveja/Bundle/DeskBundle/Controller/DefaultController.php
+++ src/Caveja/Bundle/DeskBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CavejaDeskBundle:Default:index.html.twig', []);
+        return $this->redirect($this->generateUrl('caveja_money_homepage'));
     }
 }
